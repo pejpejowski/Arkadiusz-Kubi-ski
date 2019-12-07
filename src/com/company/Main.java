8bc70985d9259717a65f04b4c4b234ad39684554
pejpejowski <pejpejowski@gmail.com>
2019-12-07 12:44:43 +0100

    master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        int x = 2, y = 0;
+        x = 12;
+        y = 4;
+        System.out.println(x+y);
 
 
     	x = y + y;
