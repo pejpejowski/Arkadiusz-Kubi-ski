a8afd46d61ca8f4d70a42b277d5b2fa18352fd79
pejpejowski <pejpejowski@gmail.com>
2019-12-07 12:22:52 +0100

    zmienne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int x = 2, y = 0;
+	x = 12;
+	y = 4;
+	System.out.println(x+y);
     }
 }
