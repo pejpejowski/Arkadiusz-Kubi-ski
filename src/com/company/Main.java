b8a66eac821bb0a21b51153a00172c68a4c4afd9
pejpejowski <pejpejowski@gmail.com>
2019-12-07 12:31:21 +0100

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int x = 2, y = 0;
+	int x = 2, y = 0, z = 0;
 	x = 8;
 	y = 2;
 	System.out.println(x+y+y);
+	z = x + y;
     }
 }
